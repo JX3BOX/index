70f9410195ac0b85dfef9dff98281bc3d14c2977
iRuxu <rx6@qq.com>
2026-04-22 14:36:28 +0800

    sync:ui

--- vue.config.js
+++ vue.config.js
@@ -68,35 +68,6 @@ module.exports = {
         proxy: buildEnvProxy(),
         allowedHosts: "all",
         port: process.env.DEV_PORT || 12028,
-        // 避免 /macro 等其它应用路由被 index SPA 接管
-        // historyApiFallback: {
-        //     rewrites: [
-        //         {
-        //             from: /^\/macro(\/.*)?$/,
-        //             to: (context) => context.parsedUrl.pathname,
-        //         },
-        //         {
-        //             from: /^\/notice(\/.*)?$/,
-        //             to: "/notice/index.html",
-        //         },
-        //         {
-        //             from: /^\/about(\/.*)?$/,
-        //             to: "/about/index.html",
-        //         },
-        //         {
-        //             from: /^\/search(\/.*)?$/,
-        //             to: "/search/index.html",
-        //         },
-        //         {
-        //             from: /^\/post(\/.*)?$/,
-        //             to: "/post/index.html",
-        //         },
-        //         {
-        //             from: /^\/jx3(\/.*)?$/,
-        //             to: "/jx3/index.html",
-        //         },
-        //     ],
-        // },
     },
 
     // 依赖包（element-plus/theme-chalk 等）会输出大量 Sass deprecation 警告
